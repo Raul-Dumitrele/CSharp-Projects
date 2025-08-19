4c0c453c209f95c55b969c9e1f54acf44bb39ad2
Raul Dumitrele <dumitreleraulvictor@gmail.com>
2025-08-19 14:18:28 +0300

    Add files via upload

---
+++ Number guessing game/Joc de ghicit un numar/Joc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Joc
+{
+    private int numardeghicit;
+    private int incercari;
+
+    public void Init()
+    {
+        Random random = new Random();
+        numardeghicit = random.Next(1, 100);
+        incercari = 0;
+        Console.WriteLine("Am ales un numar intre 1 si 100.Incercati sa-l ghiciti!");
+    }
+
+    public void Joaca()
+    {
+        int ghicit;
+        do
+        {
+            Console.WriteLine("Introduceti numarul ghicit: ");
+            ghicit = Convert.ToInt32(Console.ReadLine());
+            incercari++;
+
+            if (ghicit < numardeghicit)
+            {
+                Console.WriteLine("Numarul este prea mic!Incercati din nou");
+            }
+            else if (ghicit > numardeghicit)
+            {
+                Console.WriteLine("Numarul este prea mare!Incercati din nou");
+            }
+            else
+            {
+                Console.WriteLine($"Felicitari!Ati ghicit numarul in {incercari} incercari!");
+            }
+        }while(ghicit != numardeghicit);
+    }
+    
+    
+}
---
+++ Number guessing game/Joc de ghicit un numar/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Joc joc = new Joc();
+        joc.Init();
+        joc.Joaca();
+    }
+}
